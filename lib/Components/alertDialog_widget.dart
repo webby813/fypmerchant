9b1227fdbe8224fb491a742aa908ebdc21b80f59
webby813 <webbyteo02@gmail.com>
2024-03-28 18:16:50 +0800

    [24] Manage Stock Mobile UI

--- lib/Components/alertDialog_widget.dart
+++ lib/Components/alertDialog_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../Color/color.dart';
 import '../Firebase/update_data.dart';
 
 class AlertDialogWidget extends StatelessWidget {
@@ -81,6 +81,106 @@ class AlertToChangeDialog extends StatelessWidget {
             ),
           ],
         )
+      ],
+    );
+  }
+}
+
+class AddUpdateDialog extends StatefulWidget {
+  final String type;
+  final onPressed;
+  const AddUpdateDialog({Key? key, required this.type, required this.onPressed});
+
+  @override
+  State<AddUpdateDialog> createState() => _AddUpdateDialogState();
+}
+
+class _AddUpdateDialogState extends State<AddUpdateDialog> {
+  String? _dropdownValue;
+  @override
+  Widget build(BuildContext context) {
+    List<String> itemStatus = ['Available', 'Unavailable'];
+    return AlertDialog(
+      backgroundColor: CustomColors.defaultWhite,
+      scrollable: true,
+      title: const Text("Product", textAlign: TextAlign.center),
+      content: SizedBox(
+        width: 800,
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Card(
+                      child: Container(
+                        width: 130,
+                        height: 125,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 105,
+                    child: DropdownButton<String>(
+                      value: _dropdownValue,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      isExpanded: true,
+                      items: itemStatus.map((String dropdownValue) {
+                        return DropdownMenuItem(
+                          value: dropdownValue,
+                          child: Text(dropdownValue,
+                            style: const TextStyle(
+                                fontSize: 13
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                      hint: const Text("Status"),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _dropdownValue = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Latte",
+                  border: InputBorder.none,
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "4.90",
+                  border: InputBorder.none,
+                ),
+              ),
+              SizedBox(
+                height: 140,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "text",
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        ElevatedButton(
+            child: Text(widget.type),
+            onPressed: () => widget.onPressed()
+        ),
       ],
     );
   }
